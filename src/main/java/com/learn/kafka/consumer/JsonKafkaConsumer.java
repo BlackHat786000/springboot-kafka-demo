b7ddd223656f671138a3f4f639aedc44aec53655
Udit Yadav <yadavudit786@gmail.com>
2024-07-14 15:43:42 +0530

    Added SSL Changes

--- src/main/java/com/learn/kafka/consumer/JsonKafkaConsumer.java
+++ src/main/java/com/learn/kafka/consumer/JsonKafkaConsumer.java
@@ -1,6 +1,6 @@
 package com.learn.kafka.consumer;
 
-import com.learn.kafka.model.User;
+import com.learn.kafka.model.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Service;
 public class JsonKafkaConsumer {
 
     @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consume(User user) {
-        log.info(String.format("Message received :: %s", user));
+    public void consume(Status status) {
+        log.info(String.format("Message received :: %s", status));
     }
 
 }
